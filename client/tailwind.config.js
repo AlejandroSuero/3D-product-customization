093f0cf1839988a999687bee6a66398fa8377f6a
AlejandroSuero <asuerome@gmail.com>
2023-10-25 18:20:11 +0200

    Added custom color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aoi: {
+          "50": "#eff9ff",
+          "100": "#daf1ff",
+          "200": "#bde7ff",
+          "300": "#90daff",
+          "400": "#5cc3fe",
+          "500": "#36a6fb",
+          "600": "#1f88f1",
+          "700": "#1870dd",
+          "800": "#1c62c2",
+          "900": "#1b4e8d",
+          "950": "#153056",
+        }
+      }
+    },
   },
   plugins: [],
 };

808ba5599208c8116106fbb8a6f8847e7ad95493
AlejandroSuero <asuerome@gmail.com>
2023-10-26 19:41:28 +0200

    Updated production backendUrl

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -5,6 +5,6 @@ export const config: ProjectConfig = {
     backendUrl: "http://localhost:8080/api/v1/dalle"
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle"
+    backendUrl: "https://threed-product-customization.onrender.com/api/v1/dalle"
   }
 };

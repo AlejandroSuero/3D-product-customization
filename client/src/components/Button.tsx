6334a854bca8336323dadfd1a8b902b4aefa5d0c
AlejandroSuero <asuerome@gmail.com>
2023-10-26 16:21:03 +0200

    Added style if isDisabled

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,16 +6,19 @@ const Button = ({
   type,
   title,
   handleClick,
-  customStyles
+  customStyles,
+  isDisabled
 }: {
   type: ButtonType,
   title: string,
   handleClick?: MouseEventHandler,
-  customStyles?: string
+  customStyles?: string,
+  isDisabled?: boolean
 }) => {
+  const disableStyle = isDisabled ? "pointer-events-none " : "";
   customStyles = type === "filled"
-    ? `${customStyles} hover:bg-aoi-700 bg-aoi-800 text-aoi-50 border-2 border-transparent transition-colors ease`
-    : `${customStyles} hover:bg-aoi-700 hover:border-transparent text-aoi-200 border-2 border-aoi-200 transition-colors ease`;
+    ? `${disableStyle}${customStyles} hover:bg-aoi-700 bg-aoi-800 text-aoi-50 border-2 border-transparent transition-colors ease`
+    : `${disableStyle}${customStyles} hover:bg-aoi-700 hover:border-transparent text-aoi-200 border-2 border-aoi-200 transition-colors ease`;
   return (
     <button onClick={handleClick} className={customStyles}>{title}</button>
   );

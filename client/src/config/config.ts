41c9ee6262e1133abdfbb1165cf9370da4ef9c3d
AlejandroSuero <asuerome@gmail.com>
2023-10-26 16:20:27 +0200

    fix: backendUrl string

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -2,7 +2,7 @@ import { ProjectConfig } from "../types";
 
 export const config: ProjectConfig = {
   development: {
-    backendUrl: "https://localhots:8080/api/v1/dalle"
+    backendUrl: "http://localhost:8080/api/v1/dalle"
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle"

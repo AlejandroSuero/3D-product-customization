eb717c0af10e9170ddcd927ecea3e8d348a48523
AlejandroSuero <asuerome@gmail.com>
2023-10-28 23:59:42 +0200

    Added vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,13 @@ import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <>
-      <Analytics />
       <Header />
       <main className="app tansitions ease-in dark:bg-gray-700 bg-gray-300">
         <Home />
         <Customizer />
         <CanvasModel />
       </main>
+      <Analytics />
     </>
   );
 }

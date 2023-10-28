aaf86f1ffb10241da229a24523a66e2c2b1478b4
AlejandroSuero <asuerome@gmail.com>
2023-10-28 23:45:39 +0200

    Added vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import CanvasModel from "./canvas";
 import Header from "./components/Header";
 import { Home, Customizer } from "./pages";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Header />
       <main className="app tansitions ease-in dark:bg-gray-700 bg-gray-300">
         <Home />
